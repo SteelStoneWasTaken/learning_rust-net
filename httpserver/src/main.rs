1c91d3dfeff859e4b9edb550c2ca8fb68e9c0fbd
SteelStoneWasTaken <82281833+SteelStoneWasTaken@users.noreply.github.com>
2024-12-22 13:21:16 -0300

    ...

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -1,26 +1,38 @@
 use warp::Filter;
+use chrono::Local;
 use std::fs;
 
 #[tokio::main]
 async fn main() {
-    let ping = warp::filters::addr::remote()
-        .and(warp::path::full())
-        .map(|ip: Option<std::net::SocketAddr>, path: warp::path::FullPath| {
-            
-            let path = format!("public{}", path.as_str());
-            println!("User connected: {}\nRequested path: {}\n--------------",
-            ip.unwrap(), path.as_str());
-            
-            match fs::read_to_string(path.as_str()) {
-                Ok(html) => warp::reply::html(html),
-                Err(_) => warp::reply::html("<h1>Error loading the page</h1>".to_string()),
-            }
-        });
+    warp::serve(
+        /* */ warp::filters::addr::remote()
+        .and( warp::path::full())
 
-    warp::serve(ping)
-        .tls()
-        .cert_path("cert.pem")
-        .key_path("key.pem")
-        .run(([0, 0, 0, 0], 3030))
-        .await;
+        .map(|ip: Option<std::net::SocketAddr>, path: warp::path::FullPath| {
+            let path = format!("public{}", path.as_str());
+            println!("[{}] {}: {}", Local::now(), ip.unwrap(), path.as_str());
+
+            match fs::read(path.as_str()) {
+                Ok(contents) => {
+                    warp::reply::with_header(
+                        contents,
+                        "content-type", match path {
+                            p if p.ends_with(".css") => "text/css",
+                            p if p.ends_with(".js")  => "application/javascript",
+                            _                        => "text/html",
+                        }
+                    )
+                },
+                Err(_) => warp::reply::with_header(
+                    "<h1>404: Not found!</h1>".as_bytes().to_vec(),
+                    "content-type",
+                    "text/html"
+                ),
+            }
+        }))
+    .tls()
+    .key_path("key.pem")
+    .cert_path("cert.pem")
+    .run(([0, 0, 0, 0], 3030))
+    .await;
 }
